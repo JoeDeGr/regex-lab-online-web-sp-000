8a4f8becaa3787e795616b953dd55d650414d74b
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-05 00:31:41 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,12 +3,19 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-untexting = text.scan(/(\b[uU][nN])|([i][n][g]\b)/)
+  i = 0
+  untexting=[]
+  while i <= text.length do
+    if (text[i].match(/\b[uU][nN]/) && text[i].match(/[i][n][g]\b/))
+      untexting << text[i]
+    end
+    i += 1
+  end
+  return untexting
 end
-return untexting
 
 def words_five_letters_long(text)
-
+  text.grep(/^\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
