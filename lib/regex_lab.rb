9c2791461a9b21d06078c3210804c1e6b4a87b3e
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-05 00:57:03 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,20 +4,10 @@ end
 
 def words_starting_with_un_and_ending_with_ing(text)
 text.scan(/\b[uU]n[a-z]*ing\b/)
-#  i = 0
-#  untexting=[]
-#  while i <= text.length do
-#    if (text.match(/\b[uU][nN]/) && text[i].match(/[i][n][g]\b/))
-#    text.each do |i|
-#      if i.scan(/\b[uU][nN]|[i][n][g]\b/)
-#    end
-#    i += 1
-#  end
-#return untexting
 end
 
 def words_five_letters_long(text)
-  text.grep(/^\w{5}/)
+  text.grep(/\b\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
